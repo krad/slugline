a4669f882756933dc0de3dc329259c4be6cbdd2c
krad <iam@krad.io>
2018-08-03 17:46:03 -0500

    Got all the building blocks in place.  Going to focus on getting them assembled properly and then will use config data to populate their fields appropriately

--- src/transmuxing/atoms.js
+++ src/transmuxing/atoms.js
@@ -110,7 +110,7 @@ export const minf = (config) => {
     [vmhd(config)], // video media information atom
     [smhd(config)], // sound media information atom
     [dinf(config)], // data information atom
-
+    [stbl(config)], // sample table atom
   ]
 }
 
@@ -156,6 +156,204 @@ export const dreference = (config) => {
     bytes.strToUint8('url '),
     new Uint8Array(1),        // version
     new Uint8Array([0, 0, 1]) // flags
+  ]
+}
+
+export const stbl = (config) => {
+  return [
+    bytes.strToUint8('stbl'),
+    [stsd(config)],             // sample description atom
+    [stts(config)],             // time to sample atom
+    [stsc(config)],             // sample to chunk atom
+    [stsz(config)],             // sample size atom
+    [stco(config)],             // chunk offset atom
+  ]
+}
+
+export const stsd = (config) => {
+  return [
+    bytes.strToUint8('stsd'),
+    new Uint8Array(1),         // version
+    new Uint8Array([0, 0, 0]), // flags
+    bytes.u32(1),              // number of entries
+    [avc1(config)],
+    [mp4a(config)],
+  ]
+}
+
+export const stts = (config) => {
+  return [
+    bytes.strToUint8('stts'),
+    new Uint8Array(1),         // version
+    new Uint8Array([0, 0, 0]), // flags
+    bytes.u32(1),              // number of entries
+  ]
+}
+
+export const avc1 = (config) => {
+  return [
+    bytes.strToUint8('avc1'),
+    new Uint8Array(6),         // reserved
+    bytes.u16(1),              // data reference index
+    bytes.u16(0),              // version
+    bytes.u16(0),              // revision level
+    bytes.u32(0),              // vendor
+    bytes.u32(0),              // temporal quality
+    bytes.u32(0),              // spatial quality
+    bytes.u16(1281),           // width
+    bytes.u16(721),            // height
+    bytes.u32(4718592),        // horizontal resolution
+    bytes.u32(4718592),        // vertical resolution
+    bytes.u32(0),              // data size
+    bytes.u16(1),              // frame count
+    new Uint8Array(1),         // compressorName size
+    new Uint8Array(31),        // padding
+    bytes.s16(24),             // depth
+    bytes.s16(-1),             // color table id
+    [avcC(config)],
+    [colr(config)],
+    [pasp(config)],
+  ]
+}
+
+export const avcC = (config) => {
+  return [
+    bytes.strToUint8('avcC'),
+    1,                                    // version
+    0x42,                                 // profile
+    0,                                    // profile compatibility
+    30,                                   // level indication
+    0b11111111,                           // nalu size
+    0b11100001,                           // sps count
+    bytes.u16(27),                        // sps length
+    [0x27, 0x4d, 0x00, 0x1f, 0x89, 0x8b,
+    0x60, 0x28, 0x02, 0xdd, 0x80, 0xb5,
+    0x01, 0x01, 0x01, 0xec, 0x0c, 0x00,
+    0x17, 0x70, 0x00, 0x05, 0xdc, 0x17,
+    0xbd, 0xf0, 0x50],                    // sps bytes
+    1,                                    // pps count
+    bytes.u16(4),                         // pps length
+    [0x28, 0xee, 0x1f, 0x20],             // pps bytes
+
+  ]
+}
+
+export const colr = (config) => {
+  return [
+    bytes.strToUint8('colr'),
+    bytes.strToUint8('nclx'),     // color parameter
+    bytes.u16(1),                 // primaries index
+    btyes.u16(1),                 // transfer function index
+    bytes.u16(1),                 // matrix index
+    new Uint8Array(1),            // unknown
+  ]
+}
+
+//    4:3 square pixels (composite NTSC or PAL) hSpacing: 1 vSpacing: 1
+//    4:3 non-square 525 (NTSC) hSpacing: 10 vSpacing: 11
+//    4:3 non-square 625 (PAL) hSpacing: 59 vSpacing: 54
+//    16:9 analog (composite NTSC or PAL) hSpacing: 4 vSpacing: 3
+//    16:9 digital 525 (NTSC) hSpacing: 40 vSpacing: 33
+//    16:9 digital 625 (PAL) hSpacing: 118 vSpacing: 81
+//    1920x1035 HDTV (per SMPTE 260M-1992) hSpacing: 113 vSpacing: 118
+//    1920x1035 HDTV (per SMPTE RP 187-1995) hSpacing: 1018 vSpacing: 1062
+//    1920x1080 HDTV or 1280x720 HDTV hSpacing: 1 vSpacing: 1
+export const pasp = (config) => {
+  return [
+    bytes.strToUint8('pasp'),
+    bytes.u32(1),             // horizontal spacing
+    bytes.u32(1),             // vertical spacing
+  ]
+}
+
+export const mp4a = (config) => {
+  return [
+    bytes.strToUint8('mp4a'),
+    new Uint8Array(6),        // reserved
+    bytes.u16(1),             // data ref index
+    bytes.u64(0),             // reserved
+    bytes.u16(2),             // channels
+    bytes.u16(16),            // sample size
+    bytes.u16(0),             // predefined
+    bytes.u16(0),             // reserved
+    bytes.u32(44100),         // sample rate
+    [esds(config)]
+  ]
+}
+
+export const esds = (config) => {
+  return [
+    bytes.strToUint8('esds'),
+    new Uint8Array(1),                  // version
+    new Uint8Array(3),                  // flags
+    0x03,                               // es desc type tag
+    new Uint8Array([0x80, 0x80, 0x80]), // 0x80 = start & 0xfe = end
+    0x22,
+    bytes.u16(1),                       // es id
+    new Uint8Array(1),                  // stream priority
+    0x04,                               // decoder config desc tag
+    new Uint8Array([0x80, 0x80, 0x80]), // 0x80 = start & 0xfe = end
+    0x14,                               // es ext desc length
+    0x40,                               // object profile indication audio == 64
+    0x15,                               // stream type
+    new Uint8Array([0x00, 0x18, 0x00]), // buffer size DB
+    bytes.u32(0),                       // max bit rate
+    bytes.u32(0),                       // avg bit rate
+    0x05,                               // decoder specific info tag
+    new Uint8Array([0x80, 0x80, 0x80]), // 0x80 = start & 0xfe = end
+    0x02,                               // desc length
+    new Uint8Array([0x12, 0x10]),       // audio specific config
+    new Uint8Array([0x06, 0x80, 0x80, 0x80]), // es ext desc tag
+    0x01,                               // sl config len
+    0x02,                               // slmp4const
+  ]
+}
+
+export const stsc = (config) => {
+  return [
+    bytes.strToUint8('stsc'),
+    new Uint8Array(1),         // version
+    new Uint8Array(3),         // flags
+    bytes.u32(0),              // number of entries
+  ]
+}
+
+export const stsz = (config) => {
+  return [
+    bytes.strToUint8('stsz'),
+    new Uint8Array(1),         // version
+    new Uint8Array(3),         // flags
+    s.u32(0),                  // sample size
+    s.u32(0),                  // number of entries
+  ]
+}
+
+export const stco = (config) => {
+  return [
+    bytes.strToUint8('stco'),
+    new Uint8Array(1),         // version
+    new Uint8Array(3),         // flags
+    s.u32(0),                  // number of entries
+  ]
+}
+
+export const mvex = (config) => {
+  return [
+    bytes.strToUint8('mvex'),
+    [trex(config)]              // track ex atom
+  ]
+}
+
+export const trex = (config) => {
+  return [
+    bytes.strToUint8('trex'),
+    new Uint8Array(1),         // version
+    new Uint8Array(3),         // flags
+    bytes.u32(1),              // track id
+    bytes.u32(1),              // sample description index
+    btyes.u32(0),              // sample duration
+    bytes.u32(0),              // sample size
+    bytes.u32(0x02000000)      // sample flags
   ]
 }
 
