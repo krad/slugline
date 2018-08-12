6462c4c4ea06eee877e9358831c532326c064083
krad <iam@krad.io>
2018-08-12 03:20:51 -0500

    Got an access unit builder going for video

--- src/transmuxing/transmuxer.js
+++ src/transmuxing/transmuxer.js
@@ -10,43 +10,44 @@ class Transmuxer {
     this.decode               = 0
   }
 
-  buildInitializationSegment(ts) {
+  buildInitializationSegment() {
     let result = []
     result.push(atoms.ftyp())
-    result.push(atoms.moov(ts.tracksConfig))
+    result.push(atoms.moov(this.currentStream.tracksConfig))
     result = result.map(a => atoms.build(a))
     return bytes.concatenate(Uint8Array, ...result)
   }
 
-  buildSequences(ts, streamType) {
-    let result    = []
-    let track     = ts.tracks.filter(t => t.streamType == streamType)[0]
-    let currentSequence
-    track.chunks.forEach(chunk => {
-
-      if (chunk.isKeyFrame) {
-        if (currentSequence) {
-          let x = atoms.moof({payload: currentSequence})
-          let y = atoms.build(x)
-          this.currentOffset = y.length + (8*4)
-
-          result.push({currentMediaSequence: this.currentMediaSequence++,
-                                    payload: currentSequence,
-                                    trackID: track.trackID,
-                                 streamType: streamType,
-                                     offset: this.currentOffset,
-                                     decode: this.decode})
-
-          this.decode += currentSequence.reduce((acc, curr) => acc + (curr.pts), 0)
-
-        }
-        currentSequence = [chunk]
-      } else {
-        if (currentSequence) {
-          currentSequence.push(chunk)
-        }
-      }
-    })
+  buildSequences(streamType) {
+    let result = []
+    // let result    = []
+    // let track     = ts.tracks.filter(t => t.streamType == streamType)[0]
+    // let currentSequence
+    // track.chunks.forEach(chunk => {
+    //
+    //   if (chunk.isKeyFrame) {
+    //     if (currentSequence) {
+    //       let x = atoms.moof({payload: currentSequence})
+    //       let y = atoms.build(x)
+    //       this.currentOffset = y.length + (8*4)
+    //
+    //       result.push({currentMediaSequence: this.currentMediaSequence++,
+    //                                 payload: currentSequence,
+    //                                 trackID: track.trackID,
+    //                              streamType: streamType,
+    //                                  offset: this.currentOffset,
+    //                                  decode: this.decode})
+    //
+    //       this.decode += currentSequence.reduce((acc, curr) => acc + (curr.pts), 0)
+    //
+    //     }
+    //     currentSequence = [chunk]
+    //   } else {
+    //     if (currentSequence) {
+    //       currentSequence.push(chunk)
+    //     }
+    //   }
+    // })
 
     return result
   }
--- tests/transmuxer-test.js
+++ tests/transmuxer-test.js
@@ -304,12 +304,16 @@ test('that we can build an media fragment', t=> {
   t.end()
 })
 
-test('writing a segment', t=> {
+test.only('writing a segment', t=> {
   const bufferA  = Uint8Array.from(asset2)
   let tsA        = TransportStream.parse(bufferA)
   let muxer     = new Transmuxer()
-  const init    = muxer.buildInitializationSegment(tsA)
-  const payload = muxer.buildMediaSegment(tsA)
+
+  muxer.currentStream = tsA
+
+  const init    = muxer.buildInitializationSegment()
+  const payload = muxer.buildMediaSegment(27)
+
   fs.appendFileSync('/tmp/chunk.mp4', new Buffer(init))
   fs.appendFileSync('/tmp/chunk.mp4', new Buffer(payload))
   // //
